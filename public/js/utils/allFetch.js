4a9d2c8d913b312ba921a61ea32180a4d4a2466b
Shehroz <xiyobidasturchi0011@gmail.com>
2025-01-10 11:43:55 +0500

    Of the folder adds allFetch

---
+++ public/js/utils/allFetch.js
@@ -0,0 +1,27 @@
+function apiFetch(uri,options={}){
+    const baseUrl = 'http://localhost:8080/api',
+        token = localStorage.getItem('token');
+    const defaultHeaders = {};
+    if (token) {
+        defaultHeaders.Authorization = `Bearer ${token}`;
+
+    }
+    return fetch(`${baseUrl}${uri}`,{
+        ...options,
+        headers: {
+            ...defaultHeaders,
+            ...options.headers
+        }
+    })
+        .then(async response => {
+        if (!response.ok) {
+            const error=new Error('http error');
+            error.data=await response.json();
+            throw error;
+        }
+        return response.json();
+    }).catch(error => {
+        throw error;
+    });
+}
+export default apiFetch;
