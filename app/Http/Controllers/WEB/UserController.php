7d33b4fe44d626d39d15e6a78da880edce85ce0b
Shehroz <xiyobidasturchi0011@gmail.com>
2025-01-11 10:07:27 +0500

    WEB add UserController

--- app/Http/Controllers/WEB/UserController.php
+++ app/Http/Controllers/WEB/UserController.php
@@ -4,8 +4,21 @@ namespace App\Http\Controllers\WEB;
 
 class UserController
 {
-    public function home()
+    public function home():void
     {
         view('dashboard/home');
     }
+    public function create_quiz():void
+    {
+        view('dashboard/create-quiz');
+    }
+    public function my_quizzes():void
+    {
+        view('dashboard/my_quizzes');
+    }
+    public function statistic():void
+    {
+        view('dashboard/statistic');
+    }
+
 }
