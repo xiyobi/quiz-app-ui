7990276ae46c57be138ac1a74f750c8bbb0de9f2
Shehroz <xiyobidasturchi0011@gmail.com>
2025-01-17 11:22:19 +0500

    PATCH web route

--- routes/web.php
+++ routes/web.php
@@ -12,9 +12,9 @@ Router::get('/login', [HomeController::class, 'login']);
 Router::get('/register', [HomeController::class, 'register']);
 
 Router::get('/dashboard', [UserController::class, 'home']);
-Router::get('/quizzes', [UserController::class, 'quizzes']);
-Router::get('/create_quiz', [UserController::class, 'create_quiz']);
-Router::get('/statistic', [UserController::class, 'statistic']);
+Router::get('/dashboard/quizzes', [UserController::class, 'quizzes']);
+Router::get('/dashboard/create_quiz', [UserController::class, 'create_quiz']);
+Router::get('/dashboard/statistic', [UserController::class, 'statistic']);
 
 
 Router::notFound();
