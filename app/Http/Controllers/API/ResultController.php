f4de5ba819babdb71f7cfa5f554d05e44197a989
Shehroz <xiyobidasturchi0011@gmail.com>
2025-02-02 22:04:46 +0500

    PATCH

--- app/Http/Controllers/API/ResultController.php
+++ app/Http/Controllers/API/ResultController.php
@@ -19,6 +19,29 @@ class ResultController
         $quiz=(new Quiz())->find($resultItems['quiz_id']);
         if($quiz) {
             $result = new Result();
+            $userResult = $result->getUserResults(Auth::user()->id,$quiz->id);
+            if ($userResult){
+                $startedAt = strtotime($userResult->started_at);
+                $finishedAt =strtotime($userResult->finished_at);
+                $diff=abs($finishedAt-$startedAt);
+                $years=floor($diff/(365*60*60*24));
+                $months = floor(($diff-$years*365*60*60*24)/(30*60*60*24));
+                $days=floor(($diff-$years*365*60*60*24)-$months*30*60*60*24)/(60*60);
+                $hours=floor(($diff -$years*365*60*60*24-$months*30*60*60*24-$days*60*60*24)/(60*60));
+                apiResponse([
+                    'errors'=>[
+                        'massage'=>'You have already taken this quiz'
+                    ],
+                    'data'=>[
+                        'result'=>[
+                            'id'=>$userResult->id,
+                            'quiz'=>$quiz,
+                            'started_at'=>$userResult->started_t,
+                            'time_taken'=>floor(($diff-$years*365*60*60*24-$months*30*60*60*24-$days*60*60*24)/60)
+                        ]
+                    ]
+                ],400);
+            }
             $resultData=$result->create(
                 Auth::user()->id,
                 $quiz->id,
