83e53fed43aeb94caafcae8826c5340f608e5864
Shehroz <xiyobidasturchi0011@gmail.com>
2025-02-04 19:22:32 +0500

    Patch

--- bootstrap.php
+++ bootstrap.php
@@ -1,6 +1,5 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('error_reporting', E_ALL);
+
 require 'vendor/autoload.php';
 
 date_default_timezone_set('Asia/Tashkent');
--- helpers.php
+++ helpers.php
@@ -1,21 +1,35 @@
 <?php
+//$arr = ['user'=>'Axror', 'password'=>'1234'];
+//
+//var_dump($arr);
+//
+//extract($arr);
+//
+//echo $user;
+//echo $password;
 
 use JetBrains\PhpStorm\NoReturn;
 
-function view($page, $data = []): void
-{
+function view (string $page, array $data = []): void {
     extract($data);
-    require 'resources/views/'.$page. '.php';
+    require 'resources/views/' . $page . '.php';
     exit();
 }
-#[NoReturn] function redirect(string $url): void
-{
-    header("Location: $url");
+
+function assets ($fileName): string {
+    return $_ENV['APP_URL'] . '/public' . $fileName;
+}
+function components (string $component, array $data = []): void {
+    extract($data);
+    require 'resources/views/components/' . $component . '.php';
+}
+#[NoReturn] function redirect (string $url): void {
+    header('Location: ' . $url);
     exit();
 }
-#[NoReturn] function apiResponse($data, $status=200): void
-{
-    header("Content-Type: application/json");
+
+#[NoReturn] function apiResponse ($data, $status=200): void {
+    header('Content-Type: application/json');
     http_response_code($status);
     echo json_encode($data);
     exit();
