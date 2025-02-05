90e07d1684caf48ac2f0fc78a0a03cb56e76281c
Shehroz <xiyobidasturchi0011@gmail.com>
2025-02-05 12:21:03 +0500

    PATCH: login

--- resources/views/auth/login.php
+++ resources/views/auth/login.php
@@ -6,6 +6,8 @@
     <title>Login - Quiz App</title>
     <link rel="stylesheet" href="<?php echo assets('/css/tailwind.css')?>">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.0.2/tailwind.min.css" rel="stylesheet">
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body class="bg-gray-50">
 <div class="min-h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
