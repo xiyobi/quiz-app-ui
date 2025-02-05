67acdc71be6324e311d328dc8868e261a903c3ba
Shehroz <xiyobidasturchi0011@gmail.com>
2025-02-05 10:28:01 +0500

    PARCH

--- resources/views/auth/login.php
+++ resources/views/auth/login.php
@@ -44,7 +44,7 @@
                 </div>
             </div>
 
-            <div>    // abdullajon@gmail.com 123456789
+            <div>
                 <button type="submit"
                         class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                     Sign in
--- resources/views/errors/404.php
+++ resources/views/errors/404.php
@@ -6,7 +6,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>No Found</title>
-    <link rel="stylesheet" href="css/output.css">
+    <link rel="stylesheet" href="<?php echo assets('/css/tailwind.css')?>">
 </head>
 <body>
     <div>
